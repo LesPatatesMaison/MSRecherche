13f5c7961f918a64f86ce1cea7a649e8ad96b3f8
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-07-21 09:47:04 +0200

    init++

---
+++ src/test/java/org/patatesmaison/msrecherche/MsRechercheApplicationTests.java
@@ -0,0 +1,13 @@
+package org.patatesmaison.msrecherche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRechercheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
